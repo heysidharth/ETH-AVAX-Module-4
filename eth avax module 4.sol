4aa7c4180665331fe99d71ea96859ec13d93a203
Sidharth Samantaray <111923974+heysidharth@users.noreply.github.com>
2023-07-28 13:55:52 +0530

    eth avax module 4.sol

--- eth avax module 4.sol
+++ eth avax module 4.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
 contract DegenToken is ERC20, Ownable {
 
@@ -16,10 +17,34 @@ contract DegenToken is ERC20, Ownable {
         function mint(address to, uint256 amount) public onlyOwner {
             _mint(to, amount);
     }
+        //redeemshop
+        function openredeemshop() external pure returns (string memory) {
+        return "(1) UPGRADE CHARACTER FOR 400 DGN. (2) GET AUCTION FOR 200 DGN. (3) BUY NEW STADIUM FOR 100 DGN. (4) BUY NEW BAT FOR 50 DGN";
+    }
         //redeemtoken
-        function redeemTokens(uint256 amount) public {
-        require(balanceOf(msg.sender) >= amount, " sufficient balance ");
-        _burn(msg.sender, amount);
+        function redeem(uint select) external payable {
+        require(select>0 && select<5,"PLEASE TYPE 1,2,3 OR 4 ONLY");
+        uint DGN;
+        if (select ==4){
+            DGN = 50;
+            _burn(msg.sender, DGN);
+            console.log("redeem successfull");
+        }
+        else if(select ==3){
+            DGN = 100;
+            _burn(msg.sender, DGN);
+            console.log("redeem successfull");
+        }
+        else if(select ==2){
+            DGN = 200;
+            _burn(msg.sender, DGN);
+            console.log("redeem successfull");
+        }
+        else {
+            DGN = 400;
+            _burn(msg.sender, DGN);
+            console.log("redeem successfull");
+        }    
     }
 
         //transfertoken
@@ -28,13 +53,15 @@ contract DegenToken is ERC20, Ownable {
         _transfer(_msgSender(),recipient,amount);
         return true;
     }
+
         //burnfunction
         function burn(uint amount) public {
         _burn(msg.sender, amount);
         emit burntoken(msg.sender, amount);
     }
+
        //checkbalance
         function checkBalance(address account) public view returns (uint256){
         return balanceOf(account);
-    }
+    }   
 }
